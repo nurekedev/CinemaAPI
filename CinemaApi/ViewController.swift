428c3ba1c160bb93a33bf54674cb7e8cdf7c9f15
Nureke <Snurekeee03@gmail.com>
2023-07-28 15:13:30 +0600

    Initial Commit

---
+++ CinemaApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CinemaApi
+//
+//  Created by Nurzhan Saktaganov on 28.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
